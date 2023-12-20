b0f87051e18412c680fd3daac3dbba28fe1823fc
Levafin <Komleonid@list.ru>
2023-12-20 19:24:37 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    //1. Составить программу вывода любого числа любое заданное число раз.
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 14; i++) {
+            System.out.print(33 + " ");
+        }
+    }
+
+}
---
+++ src/Task4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task4 {
+    //4. Вычислить сумму 1+1/2+1/3+...+1/n.
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите n>0:");
+        double n=sc.nextDouble();
+        double sum=0;
+        for (double i=1; i<=n; i++)
+            sum=sum+(1/i);
+        System.out.println("Сумма 1/1..1/n = "+sum);
+    }
+}
